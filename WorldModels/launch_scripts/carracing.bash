b0577e2f1803d345e2cbe4bc93268eaea181fe9d
Pantelis Monogioudis <monogiou@njit.edu>
2021-04-14 06:47:05 -0400

    updated python to python3

--- WorldModels/launch_scripts/carracing.bash
+++ WorldModels/launch_scripts/carracing.bash
@@ -2,7 +2,7 @@ CONFIG_PATH=configs/carracing.config
 for i in `seq 1 64`;
 do
   echo worker $i
-  CUDA_VISIBLE_DEVICES=-1 xvfb-run -a -s "-screen 0 1400x900x24 +extension RANDR" -- python extract.py -c $CONFIG_PATH &
+  CUDA_VISIBLE_DEVICES=-1 xvfb-run -a -s "-screen 0 1400x900x24 +extension RANDR" -- python3 extract.py -c $CONFIG_PATH &
   sleep 1.0
 done
 wait
