8fb42d246fe3c0dfd7aad96cc0f1e73715bc9c96
Pantelis Monogioudis <monogiou@njit.edu>
2021-04-14 10:40:37 +0000

    updated number of workers in config

--- WorldModels/launch_scripts/carracing.bash
+++ WorldModels/launch_scripts/carracing.bash
@@ -1,5 +1,5 @@
 CONFIG_PATH=configs/carracing.config
-for i in `seq 1 64`;
+for i in `seq 1 4`;
 do
   echo worker $i
   CUDA_VISIBLE_DEVICES=-1 xvfb-run -a -s "-screen 0 1400x900x24 +extension RANDR" -- python extract.py -c $CONFIG_PATH &
